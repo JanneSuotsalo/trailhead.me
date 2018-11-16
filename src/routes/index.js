91d60107468687e9f121f2fae366409ac52db786
Mikko Romo <mikko1.romo@gmail.com>
2018-11-16 14:21:19 +0200

    added anonymous user feed and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const helloWorldRoutes = require('./helloWorldRoutes');
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
+const feedRoutes = require('./feedRoutes');
 
-module.exports = [helloWorldRoutes, userRoutes, commentRoutes];
+module.exports = [helloWorldRoutes, userRoutes, commentRoutes, feedRoutes];
