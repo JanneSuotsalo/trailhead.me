04e6c52b727d232f27083c54c13d9357c3126f45
Mikko Romo <mikko1.romo@gmail.com>
2018-11-19 13:12:31 +0200

    added changes to feeds

---
+++ src/modules/feed/personalFeed.js
@@ -0,0 +1,30 @@
+const { request } = require('modules/util');
+const joi = require('joi');
+
+// prettier-ignore
+const schema = joi.object({
+    page: joi.number().integer().min(0).required()
+  });
+
+module.exports = request(async (trx, req, res) => {
+  // Validate the incoming request with Joi
+  const valid = joi.validate(req.body, schema);
+  if (valid.error) {
+    return { status: 'validation error', error: valid.error };
+  }
+
+  if (!req.session.isPopulated) {
+    return {
+      status: 'forbidden',
+      error: 'Invalid session, please login again...',
+    };
+  }
+
+  //Feed for posts by the loggged in user
+  const [posts] = await trx.execute(
+    'SELECT * FROM post, follower WHERE follower.followerID = ? AND follower.userID = post.userID ORDER BY post.createdAt DESC LIMIT ?, ?;',
+    [req.session.userID, Number(req.body.page) * 10, 10]
+  );
+
+  return { status: 'ok', posts };
+});
--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -1,7 +1,9 @@
 const feed = require('modules/feed/feed');
 const userFeed = require('modules/feed/userFeed');
+const personalFeed = require('modules/feed/personalFeed');
 
 module.exports = app => {
   app.post('/feed', feed);
   app.post('/userFeed', userFeed);
+  app.post('/personalFeed', personalFeed);
 };
