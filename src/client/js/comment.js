894bbbbe2f45c642b0b73a9eeffffa24fb8ef3ca
JanneSuotsalo <janne.suotsalo@gmail.com>
2018-11-26 14:48:59 +0200

    added comment.js , modified comment UI

--- src/client/js/comment.js
+++ src/client/js/comment.js
@@ -1,1 +1,38 @@
-console.log('just a comment test');
+'use strict';
+
+const button = document.getElementById('commentButton');
+const textArea = document.getElementById('commentTextarea');
+
+// Create a new comment
+button.addEventListener('click', evt => {
+  fetch('../comment', {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text: textArea.value }),
+  })
+    .then(data => data.json())
+    .then(json => {
+      console.log(json);
+    });
+
+  textArea.value = '';
+});
+
+// Delete a comment
+const deleteComment = document.getElementsByClassName('deleteComment');
+
+fetch('list', {
+  method: 'post',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({}),
+})
+  .then(data => data.json())
+  .then(json => {
+    console.log(json);
+  });
