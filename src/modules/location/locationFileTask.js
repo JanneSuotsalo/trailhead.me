fee3846d5f616cb17159d032981e6fb97f9ce63a
Tuomas Pöyry <tuomas@munkkeli.io>
2018-11-27 19:33:22 +0200

    Fixed location image job

--- src/modules/location/locationFileTask.js
+++ src/modules/location/locationFileTask.js
@@ -1,4 +1,4 @@
-const https = require('https');
+const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
@@ -7,63 +7,87 @@ const fileModule = require('modules/file');
 
 const root = path.join(__dirname, '../../../storage');
 
+let lastTime = null;
+
+/**
+ * Helper to download a file in a promise
+ * @param {object} location
+ */
+const downloadFilePromise = (location, filePath) =>
+  new Promise((resolve, reject) => {
+    const { x, y } = location.coordinates;
+    const stream = fs.createWriteStream(filePath);
+
+    stream.on('finish', resolve);
+    stream.on('error', reject);
+
+    const url = `http://geo.trailhead.me/${x}/${y}/14`;
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${process.env.GEO_KEY}`,
+      },
+    };
+
+    http
+      .get(url, options, response => response.pipe(stream))
+      .on('error', reject);
+  });
+
+/**
+ * Loop to get background images for locations from geo.trailhead.me.
+ * Getting one background image might take over 30 seconds, so it must be done in the background.
+ *
+ * Code for the background image generator running in Heroku can be found here:
+ * https://github.com/Munkkeli/trailhead-geo
+ */
 const loadGeoFile = () =>
   transaction(async trx => {
+    lastTime = Date.now();
+
     const [list] = await trx.query(`
-    SELECT
-      l.locationID,
-      l.coordinates
-    FROM location l
-    WHERE
-      NOT EXISTS (SELECT fileID FROM locationFile lf WHERE lf.locationID = l.locationID)
-    LIMIT 1
-`);
-    list.forEach(x => {
-      console.log('Loading GEO file for location', x.locationID);
+      SELECT
+        l.locationID,
+        l.coordinates
+      FROM location l
+      WHERE
+        NOT EXISTS (SELECT fileID FROM locationFile lf WHERE lf.locationID = l.locationID)
+      LIMIT 4
+    `);
 
-      const filePath = path.join(root, '/temp', `geo-${x.locationID}`);
-      const stream = fs.createWriteStream(filePath);
+    for (const location of list) {
+      console.log('Loading GEO file for location', location.locationID);
 
-      stream.on('finish', async () => {
-        const fileID = await fileModule.process(trx, {
-          mimetype: 'image/png',
-          path: filePath,
-          filename: `geo-${x.locationID}`,
-          originalname: `geo-${x.locationID}`,
-        });
+      const fileName = `geo-${location.locationID}`;
+      const filePath = path.join(root, '/temp', fileName);
 
-        await trx.execute(
-          `INSERT INTO locationFile (locationID, fileID) VALUES (?, ?)`,
-          [x.locationID, fileID]
-        );
+      await downloadFilePromise(location, filePath);
 
-        console.log(
-          'Loading GEO file for location',
-          x.locationID,
-          'successfull!'
-        );
+      const fileID = await fileModule.process(trx, {
+        mimetype: 'image/png',
+        path: filePath,
+        filename: fileName,
+        originalname: fileName,
       });
 
-      const url = `https://trailhead-geo.herokuapp.com/${x.coordinates.x}/${
-        x.coordinates.y
-      }/14`;
+      await trx.execute(
+        `INSERT INTO locationFile (locationID, fileID) VALUES (?, ?)`,
+        [location.locationID, fileID]
+      );
 
-      https
-        .get(
-          url,
-          {
-            headers: {
-              Authorization: `Bearer ${process.env.GEO_KEY}`,
-            },
-          },
-          response => response.pipe(stream)
-        )
-        .on('error', err => {
-          console.error('Failed to get GEO image for location', x.locationID);
-        });
-    });
+      console.log(
+        'Loading GEO file for location',
+        location.locationID,
+        'successfull!'
+      );
+    }
+
+    lastTime = null;
   });
 
 module.exports = () => {
-  setTimeout(loadGeoFile, 20000);
+  setInterval(() => {
+    // If the last operation has finished, or more than 5 minutes has passed
+    if (!lastTime || Date.now() - lastTime > 60000 * 5) loadGeoFile();
+  }, 20000);
 };
