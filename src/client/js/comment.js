3a18c23f1e8d3acd46c9a40e4da87ec3f1bac847
JanneSuotsalo <janne.suotsalo@gmail.com>
2018-11-27 11:51:27 +0200

    Added delete icon for comments

--- src/client/js/comment.js
+++ src/client/js/comment.js
@@ -2,6 +2,58 @@
 
 const button = document.getElementById('commentButton');
 const textArea = document.getElementById('commentTextarea');
+let commentList = document.getElementById('commentList');
+let comment = '';
+
+// Create comment list
+
+// Fetch the the list of comments
+fetch('list', {
+  method: 'post',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({}),
+})
+  .then(data => data.json())
+  .then(json => {
+    const showList = html => {
+      commentList.innerHTML = html;
+    };
+
+    json.list.forEach(element => {
+      console.log(element);
+
+      // Create a delete comment icon
+      const commentDelete = () => {
+        if (window.user.username === element.userName) {
+          return '<p id="deleteComment"><span class="mdi mdi-close-circle-outline"></span></p>';
+        } else {
+          return '';
+        }
+      };
+      // Build the html of comment list
+      comment +=
+        '<div id ="flex">' +
+        '<img src="https://i.imgur.com/MQcuk3n.jpg" alt="Profile pic" id="profilePic">' +
+        '<p id="username">' +
+        element.displayName +
+        '</p>' +
+        commentDelete() +
+        '</div>' +
+        '<p id="commentText">' +
+        element.text +
+        '</p>' +
+        '<span id="time">' +
+        element.createdAt +
+        '</span>' +
+        '<br>' +
+        '<div class="line"></div>' +
+        '<br>';
+    });
+    showList(comment);
+  });
 
 // Create a new comment
 button.addEventListener('click', evt => {
@@ -17,22 +69,5 @@ button.addEventListener('click', evt => {
     .then(json => {
       console.log(json);
     });
-
   textArea.value = '';
 });
-
-// Delete a comment
-const deleteComment = document.getElementsByClassName('deleteComment');
-
-fetch('list', {
-  method: 'post',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({}),
-})
-  .then(data => data.json())
-  .then(json => {
-    console.log(json);
-  });
